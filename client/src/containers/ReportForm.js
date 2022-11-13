e127063c2a56198e52b0ddd9cfaeebdb86bb0560
willowyang <wy7708@princeton.edu>
2022-11-13 06:33:24 -0500

    report form direction

--- client/src/containers/ReportForm.js
+++ client/src/containers/ReportForm.js
@@ -1,15 +1,13 @@
 import { Wrapper } from "../components/Wrapper";
-import { Box, Button, Link, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import { useAuth } from "../contexts/AuthContext";
 import { InputField } from "../components/InputField";
-import { collection, doc, setDoc } from "firebase/firestore"; 
-import { db } from "../firebase";
+import { Image } from "@chakra-ui/react";
+import {Link} from 'react-router-dom';
+
 
 const ReportForm = () => {
-  const submitForm = () => {
-    
-  }
 
   const { currentUser, currentUserId } = useAuth();
 
@@ -18,11 +16,11 @@ const ReportForm = () => {
       <Heading p={3}>Report Form</Heading>
       <Box shadow="md" borderWidth="1px" borderRadius="md" p={5}>
         <Formik
-          initialValues={{ email: "", password: "" }}
-          onSubmit={submitForm}>
+          initialValues={{ email: "", password: "" }}>
           {({ isSubmitting }) => (
             <Form>
                <InputField name="Description" placeholder="Description" label="Description" />
+               <Link to="/cases">
               <Button
                 type="submit"
                 colorScheme="teal"
@@ -30,10 +28,14 @@ const ReportForm = () => {
                 mt={4}
               >
                 Report
+              
               </Button>
+              </Link>
             </Form>
+            
           )}
         </Formik>
+
       </Box>
     </Wrapper>
   );
